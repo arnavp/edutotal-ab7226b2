435206b045fa35856d3c1fd4b8ff9398ec2190c8
arnavp <purohit.arnav@live.in>
2026-07-04 18:06:43 +0530

    Added required config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ preview: {
-    allowedHosts: ['edutotal-1.onrender.com']
-  } }) => ({
+export default defineConfig(({ mode
+ }) => ({
   server: {
     host: "::",
     port: 8080,
+  },
+  preview: {
+    allowedHosts: ['edutotal-1.onrender.com']
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
